1d2f028f1fc98d91f9a7563bb794ae4b1dd2f98d
Noah <navscout302@gmail.com>
2025-05-19 11:33:41 -0400

    Added resource dropping to the Interactable Resource class
    Things that inherit interactable resource can now set a "loot table" of sorts so that when the drop_resources is called it will randomly drop resources from that "table" based on provided values

--- scripts/Interactable_Resource.gd
+++ scripts/Interactable_Resource.gd
@@ -2,6 +2,8 @@ extends Node2D
 class_name Interactable_Resource
 
 var interactable_area : Area2D 
+var resource_preload_array : Array[ResourcePreloader]
+var resource_chance_array : Array[float]
 
 func _draw():
 	interactable_area = find_child("Interactable_Area")
@@ -12,3 +14,9 @@ func _process(delta):
 
 func interact():
 	print_debug("Interact")
+
+func drop_resources():
+	var rand = randf()
+	for i in range(resource_chance_array.size()):
+		if(rand <= resource_chance_array[i]):
+			resource_preload_array[i].instantiate()
