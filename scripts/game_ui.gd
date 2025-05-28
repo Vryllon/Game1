ad0b9973ea4db4df9c075ecb40c6ea10d1d3e365
Noah <navscout302@gmail.com>
2025-05-28 15:23:32 -0400

    Added Life Force bar to GUI

---
+++ scripts/game_ui.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var life_force_max = 100.00
+var life_force = life_force_max
+
+var gradient: Gradient
+
+func _ready():
+	gradient = $Life_Bar/Sprite2D.texture.get_gradient()
+
+# updates the life force of the character
+# returns false if <=0 and true otherwise
+func update_life_force(life_delta):
+	# update number
+	life_force += life_delta
+	#print_debug(life_force)
+	# update display
+	gradient.set_offset(0,life_force/life_force_max)
+	gradient.set_offset(1,life_force/life_force_max + 0.05)
+	
+	return life_force > 0
