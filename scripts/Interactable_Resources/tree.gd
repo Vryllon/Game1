0f59b669a0c1531c18d3dd5260f8e2677423237c
Noah <navscout302@gmail.com>
2025-06-05 10:30:22 -0400

    Fixed Scenes breaking
    It seems like if the scene is a modified duplicate then it will eventually corrupt for unknown reasons

--- scripts/Interactable_Resources/tree.gd
+++ scripts/Interactable_Resources/tree.gd
@@ -3,11 +3,11 @@ extends Interactable_Resource
 func _ready():
 	# Add apple to loot table
 	resource_preload_array.append(preload("res://scenes/Items/Resources/Food/apple.tscn"))
-	resource_chance_array.append(0.1)
+	resource_chance_array.append(1)
 	
 	# Add stick to loot table
 	resource_preload_array.append(preload("res://scenes/Items/Resources/stick.tscn"))
-	resource_chance_array.append(0.3)
+	resource_chance_array.append(1)
 	
 	#print_debug(resource_preload_array)
 
