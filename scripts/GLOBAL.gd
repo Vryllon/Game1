6373ff55ae5af2d66c835a0b45fbaaa8465f24af
Noah <navscout302@gmail.com>
2025-06-27 16:30:58 -0400

    Added GLOBAL class to store globals

---
+++ scripts/GLOBAL.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name GLOBAL
+
+# Item Data
+
+var stick = {"width" : 20, "height" : 2, "texture_path" : "res://art/Items/stick.tres"}
+var apple = {"width" : 30, "height" : 30, "texture_path" : "res://art/Items/apple.tres"}
