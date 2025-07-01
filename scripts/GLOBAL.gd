2fa4eb534509867bb510618389cbd8aec0819183
Noah <navscout302@gmail.com>
2025-07-01 10:26:43 -0400

    Globalized player life force

--- scripts/GLOBAL.gd
+++ scripts/GLOBAL.gd
@@ -8,11 +8,20 @@ static var item_data = {
 	"apple" : {"width" : 30, "height" : 30, "texture_path" : "res://art/Items/apple.tres", "recovery_amount" : 20}
 }
 
+
 # Interactable Resource Data
 
 static var interactable_resource_data = {
 	"tree" : {"width" : 100, "height" : 100, "texture_path" : "res://art/Interactable_Resources/tree.tres", "loot_table" : [["stick", 1], ["apple", 1]]}
 }
 
+
 # Map Data
+
 static var current_map : Node2D
+
+
+# Player Data
+
+static var life_force_max = 100
+static var life_force = life_force_max
--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-var life_force_max = 100.00
-var life_force = life_force_max
-
 var gradient: Gradient
 
 func _ready():
@@ -12,21 +9,21 @@ func _ready():
 # returns false if <=0 and true otherwise
 func update_life_force(life_delta):
 	# update number
-	life_force += life_delta
+	GLOBAL.life_force += life_delta
 	# prevent overflow
-	if life_force > life_force_max:
-		life_force = life_force_max
-	#print_debug(life_force)
+	if GLOBAL.life_force > GLOBAL.life_force_max:
+		GLOBAL.life_force = GLOBAL.life_force_max
+	#print_debug(GLOBAL.life_force)
 	
 	# update display
 	if life_delta < 0:
-		gradient.set_offset(0,life_force/life_force_max - 0.05)
-		gradient.set_offset(1,life_force/life_force_max)
+		gradient.set_offset(0,GLOBAL.life_force/GLOBAL.life_force_max - 0.05)
+		gradient.set_offset(1,GLOBAL.life_force/GLOBAL.life_force_max)
 	elif life_delta > 0:
-		gradient.set_offset(1,life_force/life_force_max)
-		gradient.set_offset(0,life_force/life_force_max - 0.05)
+		gradient.set_offset(1,GLOBAL.life_force/GLOBAL.life_force_max)
+		gradient.set_offset(0,GLOBAL.life_force/GLOBAL.life_force_max - 0.05)
 	
-	return life_force > 0
+	return GLOBAL.life_force > 0
 
 func test():
 	print_debug("test")
