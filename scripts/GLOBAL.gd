e6c0883a9098119bced0563a2f1b878d39b5e3ae
Noah <navscout302@gmail.com>
2025-07-01 10:22:55 -0400

    Changed Stick Texture
    Made stick more visible in the inventory by giving it a white border

--- scripts/GLOBAL.gd
+++ scripts/GLOBAL.gd
@@ -4,7 +4,7 @@ class_name GLOBAL
 # Item Data
 
 static var item_data = {
-	"stick" : {"width" : 20, "height" : 2, "texture_path" : "res://art/Items/stick.tres", "recovery_amount" : 0},
+	"stick" : {"width" : 20, "height" : 20, "texture_path" : "res://art/Items/stick.tres", "recovery_amount" : 0},
 	"apple" : {"width" : 30, "height" : 30, "texture_path" : "res://art/Items/apple.tres", "recovery_amount" : 20}
 }
 
