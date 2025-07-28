97a55e3c430f3e032115d4ae085b24d4894edb1b
Noah <navscout302@gmail.com>
2025-07-28 18:31:50 -0400

    Simplified Life Bar
    Simplified the lifebar by changing it to be a TextureProgressBar instead of a Gradient 2D

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,21 +1,16 @@
 extends Node2D
 
-var gradient: Gradient
+var lifebar: TextureProgressBar
 
 func _ready():
-	gradient = $Life_Bar/Sprite2D.texture.get_gradient()
+	lifebar = $Life_Bar/TextureProgressBar
 	GLOBAL.GUI = self
 
 func update_life_display():
 	# update display
-	var life_fraction = GLOBAL.life_force/GLOBAL.life_force_max
-	if life_fraction - gradient.get_offset(1) < 0:
-		gradient.set_offset(0,life_fraction - 0.05)
-		gradient.set_offset(1,life_fraction)
-	else:
-		gradient.set_offset(1,life_fraction)
-		gradient.set_offset(0,life_fraction - 0.05)
-	print_debug(gradient.offsets)
+	lifebar.max_value = GLOBAL.life_force_max
+	lifebar.value = GLOBAL.life_force
+	
 
 func test():
 	print_debug("test")
