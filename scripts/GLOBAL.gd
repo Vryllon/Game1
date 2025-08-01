767242692056737adab0db6eecb377e15f05ed4e
Noah <navscout302@gmail.com>
2025-08-01 12:37:45 -0400

    Added Rocks

--- scripts/GLOBAL.gd
+++ scripts/GLOBAL.gd
@@ -39,14 +39,16 @@ static func end_game():
 
 static var item_data = {
 	"stick" : {"width" : 20, "height" : 20, "texture_path" : "res://art/Items/stick.tres", "recovery_amount" : 0},
-	"apple" : {"width" : 30, "height" : 30, "texture_path" : "res://art/Items/apple.tres", "recovery_amount" : 20}
+	"apple" : {"width" : 30, "height" : 30, "texture_path" : "res://art/Items/apple.tres", "recovery_amount" : 20},
+	"stone" : {"width" : 20, "height" : 20, "texture_path" : "res://art/Items/stone.tres", "recovery_amount" : 0}
 }
 
 
 # Interactable Resource Data
 
 static var interactable_resource_data = {
-	"tree" : {"width" : 100, "height" : 100, "texture_path" : "res://art/Interactable_Resources/tree.tres", "loot_table" : [["stick", 1], ["apple", 1]]}
+	"tree" : {"width" : 100, "height" : 100, "texture_path" : "res://art/Interactable_Resources/tree.tres", "loot_table" : [["stick", 1], ["apple", 1]]},
+	"rock" : {"width" : 80, "height" : 80, "texture_path" : "res://art/Interactable_Resources/rock.tres", "loot_table" : [["stone", 1]]}
 }
 
 
--- scripts/map_scene.gd
+++ scripts/map_scene.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 var interactable_resource_preload = preload("res://scenes/Iteractable_Resources/Interactable_Resource.tscn")
 
-var tree_num = 10
+var tree_num = 8
+var boulder_num = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#$Sprite2D.set_self_modulate(Color(randf(),randf(),randf(),1))
+	spawn_interactable_resource("rock", boulder_num)
 	spawn_interactable_resource("tree", tree_num)
 
 func spawn_interactable_resource(type, amount):
