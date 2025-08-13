1d7f3065d4aeced456c274755c58cb40ded1c29a
Noah <navscout302@gmail.com>
2025-08-13 18:43:28 -0400

    Added enemy targeting
    Enemy now follows player (tested with no obstacles)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,20 @@
 extends CharacterBody2D
 
+var speed = 200.0
+
 func _ready():
 	$AnimatedSprite2D.play("default")
+
+func _physics_process(delta):
+	navigate_to_player(delta)
+
+func navigate_to_player(delta):
+	# set target position to the player position
+	$NavigationAgent2D.target_position = GLOBAL.player.global_position
+	
+	# get the next position to navigate to to get to the player
+	var navigation_position = $NavigationAgent2D.get_next_path_position()
+	
+	# set velocity in the direction of the target position
+	velocity = (navigation_position - global_position).normalized() * speed
+	move_and_slide()
