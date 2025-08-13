47493e3502b1964594ee5692a21effaaeec0d919
Noah <navscout302@gmail.com>
2025-08-13 17:54:42 -0400

    Added enemy spawn
    1 enemy now spawns randomly on each discovered map scene

--- scripts/map_scene.gd
+++ scripts/map_scene.gd
@@ -1,19 +1,31 @@
 extends Node2D
 
-var interactable_resource_preload = preload("res://scenes/Iteractable_Resources/Interactable_Resource.tscn")
+var interactable_resource_preload = load("res://scenes/Iteractable_Resources/Interactable_Resource.tscn")
+var enemy_preload = load("res://scenes/enemy.tscn")
 
 var tree_num = 8
 var boulder_num = 2
+var enemy_num = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#$Sprite2D.set_self_modulate(Color(randf(),randf(),randf(),1))
-	spawn_interactable_resource("rock", boulder_num)
-	spawn_interactable_resource("tree", tree_num)
+	#spawn_interactable_resource("rock", boulder_num)
+	#spawn_interactable_resource("tree", tree_num)
+	spawn_enemy(enemy_num)
 
 func spawn_interactable_resource(type, amount):
 	for i in range(amount):
 		var ir = interactable_resource_preload.instantiate()
-		ir.position = Vector2(randi_range(100,1052), randi_range(100,548))
+		set_random_position(ir)
 		ir.initialize(type)
 		add_child(ir)
+
+func spawn_enemy(amount):
+	for i in range(amount):
+		var enemy = enemy_preload.instantiate()
+		set_random_position(enemy)
+		add_child(enemy)
+
+func set_random_position(object):
+	object.position = Vector2(randi_range(100,1052), randi_range(100,548))
